b7060687de40ac2f3a566b2fa66f39fab78b5b3f
Brian Angulo <bangulo219@gmail.com>
2023-03-08 23:40:49 -0600

    update: adding token unit tests

--- tests/lexer_tests.cc
+++ tests/lexer_tests.cc
@@ -5,9 +5,11 @@
 
 extern "C" {
 #include "../lexer/tokens.h"
-};
+}
 
-TEST(Lexer, TokenArrBuilderReturnsDefault) {
+#define TokenPosArrSize 2
+
+TEST(Tokens, TokenArrBuilderReturnsDefault) {
     size_t testSize = sizeof (Token) * 2;
     TokensDynamicArray testArr = buildNewArray(testSize);
 
@@ -24,4 +26,44 @@ TEST(Lexer, TokenArrBuilderReturnsDefault) {
     // freeing resources
     deleteTokenArray(testArr);
     deleteTokenArray(defaultArr);
+}
+
+TEST(Tokens, CreateTokenBuildsExpected) {
+    Token expectedToken;
+    expectedToken.type = TT_LBRACE;
+    int test[TokenPosArrSize] = {0, 2};
+    char foo[] = "foo";
+    expectedToken.position = test;
+    expectedToken.value = foo;
+    expectedToken.valueLength = 3;
+    Token testToken = createToken(expectedToken.type, expectedToken.value, expectedToken.position);
+
+    ASSERT_EQ(expectedToken.valueLength, testToken.valueLength);
+    EXPECT_STREQ(testToken.value, expectedToken.value);
+    ASSERT_EQ(expectedToken.type, testToken.type);
+    // asserting Token::position equality
+    for (int i = 0; i < TokenPosArrSize ; ++i) {
+        ASSERT_EQ(expectedToken.position[i], testToken.position[i]);
+    }
+}
+
+TEST(Tokens, TokenAppendingAsExp) {
+    size_t size = sizeof (Token) * 2;
+    TokensDynamicArray testArr = buildNewArray(size);
+    int testPos[TokenPosArrSize] = {0, 2};
+    char value[] = "foo";
+    Token testToken = createToken(TT_LBRACE, value, testPos);
+    uint32_t appendedIdx = appendToken(&testArr, testToken);
+
+    // index should be zero since array was empty to begin with
+    ASSERT_EQ(appendedIdx, 0);
+    // token equality check now
+    Token tokenInArr = testArr.array[0];
+    ASSERT_EQ(tokenInArr.type, testToken.type);
+    ASSERT_EQ(tokenInArr.valueLength, testToken.valueLength);
+    EXPECT_STREQ(tokenInArr.value, testToken.value);
+    // asserting Token::position equality
+    for (int i = 0; i < TokenPosArrSize ; ++i) {
+        ASSERT_EQ(tokenInArr.position[i], testToken.position[i]);
+    }
 }
